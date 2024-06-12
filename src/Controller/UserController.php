b6a9743499c0830378dff89138c4ced34349ae82
Christophe Tesconi <christophe@MacBook-Air-de-Christophe.local>
2024-06-12 15:49:57 +0200

    FixturesUserOk

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
     #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'users' => $userRepository->findAll(),
         ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,14 +2,38 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        
+        $faker = Factory::create('fr_FR');
+
+        for ($u = 0; $u < 5; $u++) {
+            $user = new User;
+            $user->setEmail($faker->unique()->safeEmail());
+            // $user->setRoles($faker->shuffle(array('admin, user')));
+            $plaintextPassword = '1234';
+            $hashedPassword = $this->passwordHasher->hashPassword($user, $plaintextPassword);
+            $user->setPassword($hashedPassword);
+            $user->setName($faker->unique()->name());
+            $user->setScores($faker->numberBetween(10, 45000));
+            // $user->setComments($faker->text(50));
+
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
